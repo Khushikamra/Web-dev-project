a7e7ce0e187649b082a708807208c39e5556dd96
Khushi <khushikamrakk@gmail.com>
2024-08-21 13:21:32 +0530

    Adding new files

---
+++ JS/index.js
@@ -0,0 +1,34 @@
+console.log("Hello World");
+// alert("me");
+var number1= 5;
+var number2= 3;
+console.log(number1+number2);
+
+//datatypes in javascript
+//number
+var num1=90;
+var num2=90;
+
+//string
+var str1="hello khushi";
+var str2="hey kamra";
+
+//objects
+var marks={
+    khushi: 33,
+    kamra: 90,
+    dhruvi: 99.9999
+}
+
+//booleans
+var a =true;
+var b=false;
+console.log(a,b)
+;
+
+//undefined
+var und;
+console.log(und);
+
+var n=null;
+console.log(n);
